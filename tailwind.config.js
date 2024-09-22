f7734e3dc2f17937601657d684c20c2974135209
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-22 12:17:05 +0530

    Configuration + Assets

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F195F0',
+        secondary: '#FA56F7',
+      },
+      fontFamily: {
+        inter: ["Inter", "system-ui"],
+        gowun: ["Gowun Dodum", "sans-serif"],
+        nasa: ["Nasa", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
