88aab0412152fa6d307579361cca8814370f59d1
Saket Aryan <94069182+whysosaket@users.noreply.github.com>
2024-09-22 17:16:44 +0530

    Animated Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,34 @@
 import { IoMenu } from "react-icons/io5";
 import { CgMenuGridO } from "react-icons/cg";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between align-middle">
-        <div className="my-auto">
-            <div className="absolute -left-32 -top-32 -z-10 w-60 h-60 bg-p2 rounded-full"></div>
-            <CgMenuGridO size={25} />
-        </div>
-        <div className="hidden md:flex gap-8">
-            <button>EXPLORE</button>
-            <button>MARKETPLACE</button>
-            <button>RESOURCES</button>
-            <button>FAQ</button>
-        </div>
-        <div className="my-auto">
-            <div className="absolute right-1 md:right-3 -top-28 -z-10 w-20 h-60 bg-p2 rounded-br-[10rem]"></div>
-            <IoMenu size={25} />
-        </div>
-    </div>
-  )
-}
+    <>
+    <motion.div
+      initial={{ y: -200, opacity: 0, scale: 0 }}
+      animate={{ y: 0, opacity: 1, scale: 1 }}
+      transition={{ duration: 0.7, delay: 0.1 }}
+      className="flex justify-between align-middle"
+    >
+      <div className="my-auto">
+        <CgMenuGridO size={25} />
+      </div>
+      <div className="hidden md:flex gap-8">
+        <button>EXPLORE</button>
+        <button>MARKETPLACE</button>
+        <button>RESOURCES</button>
+        <button>FAQ</button>
+      </div>
+      <div className="my-auto">
+        
+        <IoMenu size={25} />
+      </div>
+    </motion.div>
+    <div className="absolute -left-32 -top-32 -z-10 w-60 h-60 bg-p2 rounded-full"></div>
+    <div className="absolute right-1 md:right-3 -top-28 -z-10 w-20 h-60 bg-p2 rounded-br-[10rem]"></div>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
